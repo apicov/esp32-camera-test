5f0f3ffce06f395f154a3b67aedb018ac36c06e1
pico <a.pico@posteo.de>
2025-01-16 13:24:46 +0100

    Stop tracking all private_data.h files

--- include/private_data.h
+++
@@ -1,7 +0,0 @@
-
-#define SSID "Vodafone-54EC"
-#define PASSWORD "m3pag9GLaeM4Ckdx"
-//#define SERVER_URL "http://50.116.52.213:8090/sensors/sensors"
-
-//const char* ssid = "DataScienceRetreat";
-//const char* password = "DSRinBerlin!";
